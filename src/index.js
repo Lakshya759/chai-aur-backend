630871e0da0a850b68f8f89645398d40fc35b7e8
Lakshya759 <lakshyasri759@gmail.com>
2025-10-06 20:02:57 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,67 @@
+// require('dotenv').config({path: './env'}) --> works but doesn't looks good as it is not consistent
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------HOW TO CONNECT THE DATABASE IN INDEX.JS FILE--------------------------------------------------
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// import express from "express";
+// const app=express();
+
+// (async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("Error:",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on ${process.env.PORT}`);
+//         })
+//     } catch(error){
+//         console.error("ERROR:",error);
+//         throw error;
+//     }
+// })()
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
